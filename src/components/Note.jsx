0340ac7257ead28d58d831d29cbe568088ba58fa
IAmShubhamSahay <119969435+IAmShubhamSahay@users.noreply.github.com>
2025-04-22 05:18:14 +0530

    Initial commit

---
+++ src/components/Note.jsx
@@ -0,0 +1,12 @@
+//5. Create a Note.jsx component to show a <div> element with a
+//<h1> for a title and a <p> for the content.
+import React from "react";
+function Note() {
+  return (
+    <div className="note">
+      <h1>This is the title </h1>
+      <p>This is the Content</p>
+    </div>
+  );
+}
+export default Note;
